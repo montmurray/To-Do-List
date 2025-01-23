d34fc60a6f3888746c030c8e9c78b2e361bc5ad8
Tessa Murray <tmurray@bsd220.org>
2025-01-23 07:25:38 -0800

    Initial Commit

---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Tessa Murray on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
