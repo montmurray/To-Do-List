1ee056af44cfceefbe648cf12b060ad0f7394460
Tessa Murray <tmurray@bsd220.org>
2025-01-23 07:38:23 -0800

    Redesigned list display.

--- To-Do List/ContentView.swift
+++ To-Do List/ContentView.swift
@@ -15,7 +15,14 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(toDoItems) { item in Text(item.description)
+                ForEach(toDoItems) { item in HStack {
+                    VStack(alignment: .leading, content: {
+                        Text(item.priority).font(.headline)
+                        Text(item.description)
+                    })
+                    Spacer()
+                    Text(item.dueDate, style: .date)
+                }
                 }
                 .onMove(perform: { indices, newOffset in toDoItems.move(fromOffsets: indices, toOffset: newOffset)
                 })
